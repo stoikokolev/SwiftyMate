ce1df2fc425dc957da9e4def0c9793934eb9bb5f
Stoyko Kolev <stoyko.kolev@scalefocus.com>
2022-02-06 22:38:24 +0200

    Add UIImage extensions

--- Sources/SwiftyMate/UIKit/UIView+Extensions.swift
+++ Sources/SwiftyMate/UIKit/UIView+Extensions.swift
@@ -48,4 +48,36 @@ extension UIView {
         subviews.forEach { $0.removeFromSuperview() }
     }
     
+    /// Shakes.
+    /// - Parameters:
+    ///   - count: Shakes count (default is 3).
+    ///   - duration: Shake duration (default is 0.2).
+    ///   - translation: Shake translation. Default is 5.
+    public func shake(count: Float = 3, duration: TimeInterval = 0.2, translation: Float = 5) {
+        let animation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+        animation.repeatCount = count
+        animation.duration = duration / TimeInterval(animation.repeatCount)
+        animation.autoreverses = true
+        animation.byValue = translation
+        
+        layer.add(animation, forKey: "shake")
+    }
+    
+    /// Pulses.
+    /// - Parameters:
+    ///   - count: Pulse count (default is 1).
+    ///   - duration: Pulse duration (default is 1).
+    public func pulse(count: Int = 1, duration: TimeInterval = 1) {
+        let animation = CABasicAnimation(keyPath: "opacity")
+        animation.duration = duration
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        animation.autoreverses = true
+        animation.repeatCount = Float(count)
+        
+        layer.add(animation, forKey: "pulse")
+    }
+    
 }
